90f5be8f3e8fc8b23f05f6c9c64483d8f709f8a2
Spadnana <matthewtayjk@gmail.com>
2026-01-29 14:13:03 +0800

    Added the Special Offer Class

---
+++ PRG2 ASG/PRG2 ASG/SpecialOffer.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//==========================================================
+// Student Number : S10273266D
+// Student Name : Matthew Tay
+// Partner Name : Jovan Soo
+//==========================================================
+
+public class SpecialOffer
+{
+    private string OfferCode;
+    private string OfferDesc;
+    private double Discount;
+
+    public SpecialOffer(string offerCode, string offerDesc, double discount)
+    {
+        OfferCode = offerCode;
+        OfferDesc = offerDesc;
+        Discount = discount;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{OfferCode} - {OfferDesc} ({Discount})";
+    }
+
+}
